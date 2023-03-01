ee3ac4abb91529903425f35f37b49ab03e621457
Beard <t0426419@yandex.ru>
2023-03-01 14:44:04 +0500

    Home work 3 - 1

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -19,14 +19,33 @@ bool result = CheckPalindrome(num);
 if(result) Console.WriteLine($"Число {num} являеться полиндромом");
 else Console.WriteLine($"Число {num} НЕ являеться полиндромом");
 */
+
 /*
 Задача 21
 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53
 */
+/*
+double DistanceBetweenPoints(
+    double x1, double y1, double z1,
+    double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0);
+}
 
-
+Console.Write("Введите координату x первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.Write(Math.Round(DistanceBetweenPoints(x1, y1, z1, x2, y2, z2), 2));
+*/
 
 /*
 Задача 23
@@ -34,4 +53,19 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
+*/
+/*
+void NumberCubeTable(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} в кубе равно: " + Math.Pow(count, 3));
+        count++;
+    }
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+NumberCubeTable(num);
 */
