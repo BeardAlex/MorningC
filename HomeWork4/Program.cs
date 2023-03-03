3cfbc201adbedd00860ec1432a0b215657939809
Beard <t0426419@yandex.ru>
2023-03-03 14:50:00 +0500

    Heme work end

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -22,7 +22,7 @@ Console.WriteLine(Exponentiate(num1, num2));
 /*
 Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 */
-
+/*
 int GetSumDigits(int num)
 {
     int result = 0;
@@ -37,10 +37,30 @@ int GetSumDigits(int num)
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.Write($"Сумма цифр числа {num} равна {GetSumDigits(num)}");
-
+*/
 ///////////////////////////////////////////////////////////////////////////////
 /*
 Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-6, 1, 33 -> [6, 1, 33]
+*/
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    return result;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Минимальное число массива: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное число массива: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+ShowArray(CreateRandomArray(size, minValue, maxValue));
 */
